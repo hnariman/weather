21bfe00625879f3c2a4872cd2de6f255dc8c8fd1
Nariman Huseynov <hnariman@gmail.com>
2020-06-10 23:41:06 +0400

    Updates

---
+++ precache-manifest.0dd93f6833962a37c46647e84fd76f4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955fec0bfbd127efc19b7c1b889a0f67",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "9f5a1eb96a7357b5de4a",
+    "url": "/weather/static/css/main.c241dd8e.chunk.css"
+  },
+  {
+    "revision": "f52808d16ae720f68a6d",
+    "url": "/weather/static/js/2.4dc06215.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.4dc06215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5a1eb96a7357b5de4a",
+    "url": "/weather/static/js/main.93d6e0f6.chunk.js"
+  },
+  {
+    "revision": "bdd4e2bf0ce8946d3851",
+    "url": "/weather/static/js/runtime-main.51874b67.js"
+  },
+  {
+    "revision": "409b1c5bc28c5b5cdf05cad75670bd57",
+    "url": "/weather/static/media/temple.409b1c5b.svg"
+  }
+]);
